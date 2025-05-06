f69d62b5aa5dcec44f9075d87b29e07c1c9c6dbc
mhmd <mhmdfayad1992@gmail.com>
2025-05-06 09:05:27 +0300

    add ad routes

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -4,9 +4,12 @@ import { authRoutes } from './auth';
 import { logRoutes } from './log';
 import { userActivityRoutes } from './user-activity';
 import { adminAuth } from '../../middlewares/admin-auth';
+import { adRoutes } from './ad';
+import { auth } from '../../middlewares/auth';
 
 export const routes = express.Router();
 routes.use('/auth', authRoutes);
+routes.use('/ads', auth, adRoutes);
 routes.use('/users', adminAuth, userRoutes);
 routes.use('/logs', adminAuth, logRoutes);
 routes.use('/user-activity', adminAuth, userActivityRoutes);
